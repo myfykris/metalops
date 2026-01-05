5c87ead60bf46579df3679a71d53841108cf5bab
Kris Bailey <kris@krisbailey.com>
2026-01-05 00:16:38 -0500

    Bump version to 0.1.3

--- packages/metalcore/setup.py
+++ packages/metalcore/setup.py
@@ -30,7 +30,7 @@ class CustomBuildExtension(BuildExtension):
 
 setup(
     name='metalcore',
-    version='0.1.2',
+    version='0.1.3',
     author='Kris Bailey',
     author_email='kris@krisbailey.com',
     description='Foundational Metal Linear Algebra Primitives for PyTorch',
